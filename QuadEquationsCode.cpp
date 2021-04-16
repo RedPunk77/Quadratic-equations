1a0b2d6eb451244a6abbe8950ec99bf8ec8145fa
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2021-04-16 20:46:50 +0300

    Update QuadEquationsCode.cpp

--- QuadEquationsCode.cpp
+++ QuadEquationsCode.cpp
@@ -1,4 +1,4 @@
- //Hi, this my hurble code and it helps to solve quadratic equatoins like ax^2 + bx + c
+ //Hi, this my humble code and it helps to solve quadratic equatoins like ax^2 + bx + c
 #include <iostream>
 int main()
 {
@@ -26,6 +26,7 @@ int main()
 				std::cout << " Discriminant equales 0,  1 root\n" << x;    //Of course, if discriminant equales 0, that doesn`t matter to add or to subtract 0, so 1st root (x(1)) equales to the 2nd one (x(2))
 			}                                                              //so, why we are not able to simply otput x (x(1))
 			else {
+				std::cout << "Discriminant is bigger than 0, 2 roots\n";
 				if (x < y) {
 					std::cout << "x(1)=" << x;
 					std::cout << "\n";
@@ -39,13 +40,14 @@ int main()
 			}
 		}
 	}
+
 	else {
 		std::cout << "a equales 0, no solution\n";     //VERY IMPORTANT MOMENT!!! if a=0 there is no point i the equation, because ax^2 would be equal to 0, this meanes that only bx + c remains, but it isn`t a quadratic equation at all
 		std::cout << " Write new a\n";
 		std::cin >> a;
 		d = ((b * b) - (4 * a * c));
 		if (d < 0) {
-			std::cout << ("Discriminant is less than 0, no roots");                    //So, the same tings happen
+			std::cout << ("Discriminant is less than 0, no roots");                    //So, the same things happen
 		}
 		else {
 			x = (-b + (sqrt(d))) / (2 * a);
@@ -57,6 +59,7 @@ int main()
 				std::cout << " Discriminant equales 0,  1 root\n" << x;
 			}
 			else {
+				std::cout << "Discriminant is bigger than 0, 2 roots\n";
 				if (x < y) {
 					std::cout << "x(1)=" << x;
 					std::cout << "\n";  

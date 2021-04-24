a587f70449544559a8da93c08c657bc6afa6b109
Makar Kosenko <81918633+RedPunk77@users.noreply.github.com>
2021-04-24 21:01:52 +0300

    Update ReadMe.cpp

--- ReadMe.cpp
+++ ReadMe.cpp
@@ -4,17 +4,24 @@ int main()
 {
 	int a, b, c, d;
 	double x, y;
-	std::cout << "Quadraic equation roots\n";
+	std::cout << "ax^2+bx+c=0\n";
+	std::cout << "Quadratic equation terms:\n";
 	std::cout << "a=";                                    //initilization of variables
 	std::cin >> a;
 	std::cout << "b=";
 	std::cin >> b;
 	std::cout << "c=";
 	std::cin >> c;
+	if (a == 1) {
+		std::cout <<"x^2+" << b << "x+" << c << std::endl;
+	}
+	else {
+		std::cout << a << "x^2+" << b << "x+" << c << std::endl;
+	}
 	if (a != 0) {
 		d = ((b * b) - (4 * a * c));
 		if (d < 0) {
-			std::cout << ("Discriminant is less than 0, no roots");      //if d<0 there are no real solutions , it`s a rule
+			std::cout << "Discriminant (D=" << d << ")" << " is less than 0, no roots" << std::endl;    //if d<0 there are no real solutions , it`s a rule
 		}
 		else {
 			x = (-b + (sqrt(d))) / (2 * a);     //x=x(1)
@@ -23,10 +30,10 @@ int main()
 
 
 			if (x == y) {
-				std::cout << " Discriminant equales 0,  1 root\n" << x;    //Of course, if discriminant equales 0, that doesn`t matter to add or to subtract 0, so 1st root x(1) equales to the 2nd one x(2)
-			}                                                              //so, why we are not able to simply output for example x(1)
+				std::cout << "Discriminant (D=" << d << ")" << " equales 0, 1 root" << std::endl;    //Of course, if discriminant equales 0, that doesn`t matter to add or to subtract 0, so 1st root (x(1)) equales to the 2nd one (x(2))
+			}                                                              //so, why we are not able to simply otput x (x(1))
 			else {
-				std::cout << "Discriminant is bigger than 0, 2 roots\n";
+				std::cout << "Discriminant (D=" << d << ")" << " is bigger than 0, 2 roots" << std::endl;
 				if (x < y) {
 					std::cout << "x(1)=" << x;
 					std::cout << "\n";
@@ -42,12 +49,12 @@ int main()
 	}
 
 	else {
-		std::cout << "a equales 0, no solution\n";     //VERY IMPORTANT MOMENT!!! if a=0 there is no point in the equation, because ax^2 would be equal to 0, this meanes that only bx + c remains, but it isn`t a quadratic equation at all
+		std::cout << "a equales 0, no solution\n";     //VERY IMPORTANT MOMENT!!! if a=0 there is no point i the equation, because ax^2 would be equal to 0, this meanes that only bx + c remains, but it isn`t a quadratic equation at all
 		std::cout << " Write new a\n";
 		std::cin >> a;
 		d = ((b * b) - (4 * a * c));
 		if (d < 0) {
-			std::cout << ("Discriminant is less than 0, no roots");                    //So, the same things happen
+			std::cout << "Discriminant (D=" << d << ")" << " is less than 0, no roots" << std::endl;                    //So, the same things happen
 		}
 		else {
 			x = (-b + (sqrt(d))) / (2 * a);
@@ -56,10 +63,10 @@ int main()
 
 
 			if (x == y) {
-				std::cout << " Discriminant equales 0,  1 root\n" << x;
+				std::cout << "Discriminant (D=" << d << ")" << " equales 0, 1 root" << std::endl;
 			}
 			else {
-				std::cout << "Discriminant is bigger than 0, 2 roots\n";
+				std::cout << "Discriminant (D=" << d << ")" << " is bigger than 0, 2 roots" << std::endl;
 				if (x < y) {
 					std::cout << "x(1)=" << x;
 					std::cout << "\n";  
